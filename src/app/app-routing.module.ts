e28acfa71aeec3c696445279254febed89da4a59
Shreedhar Pandey <sridhar.pandey1@gmail.com>
2020-07-20 00:05:41 -0500

    working-commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { AdminComponent } from './admin/admin.component';
+import { LoginChatComponent } from './login-chat/login-chat.component';
+import { ChatComponent } from './chat/chat.component';
+import { AdminMonitorComponent } from './admin-monitor/admin-monitor.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', pathMatch: 'full', redirectTo: 'admin'},
+  {path: 'dashboard', component: DashboardComponent},
+  {path: 'admin', component: AdminComponent},
+  {path: 'login-chat', component: LoginChatComponent},
+  {path: 'chat/:set/:group/:firstName/:lastName', component: ChatComponent},
+  {path: 'admin-monitor', component: AdminMonitorComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,40 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { HeaderComponent } from './header/header.component';
+import { AdminComponent } from './admin/admin.component';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireAuthModule } from '@angular/fire/auth'
+import { AngularFireStorageModule } from '@angular/fire/storage'
+import { AngularFireMessagingModule } from '@angular/fire/messaging';
+import { firebaseConfig } from './firebase'
+import { FormsModule } from '@angular/forms';
+import { LoginChatComponent } from './login-chat/login-chat.component';
+import { ChatComponent } from './chat/chat.component';
+import { MaterialModule } from './material/material.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AdminMonitorComponent } from './admin-monitor/admin-monitor.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    HeaderComponent,
+    AdminComponent,
+    LoginChatComponent,
+    ChatComponent,
+    AdminMonitorComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireAuthModule,
+    AngularFireStorageModule,
+    AngularFireMessagingModule,
+    FormsModule,
+    MaterialModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
